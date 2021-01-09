6b4374f00c32fd29b92594c4560ed00b2b4a7d82
Vicky Kumar <77146574+vkyvinay@users.noreply.github.com>
2021-01-09 12:20:18 +0530

    Add files via upload

---
+++ healthprogramer.py
@@ -0,0 +1,24 @@
+"""import time
+while(True):
+    srt=input("Press S to START and c to CLOSE")
+    if srt=='s':
+        print(time.time())
+
+    elif srt=='c':
+        print("no")
+    else:
+        print("*WRONG INPUT* \nENTER AGAIN.")"""
+
+
+"""print("Enter the time period in Hour")
+from pygame import *
+mixer.init() 
+mixer.music.load('Nadaan.mp3.')
+mixer.music.set_volume(100)
+
+mixer.music.play(1,10)"""
+
+
+#while mixer.music.get_busy():
+#    time.clock().tick(20)
+
